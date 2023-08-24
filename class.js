b35e09aa13d84d0f0cb8441f4c143d3dbd724de1
Md Asadul Islam Nayeem <asadulnayeem99@gmail.comm>
2023-08-24 10:21:50 +0600

    nothing

---
+++ class.js
@@ -0,0 +1,9 @@
+class Person {
+  constructor(firstname, lastname, salary) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.salary = salary;
+  }
+}
+const heroPerson = new Person("Rahim", "Uddin", 12000);
+console.log(heroPerson);
